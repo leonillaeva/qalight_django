4c2a4eed76dce465ded1c5addb095a885b9ce7b1
Liza Herasymenko <leonillaeva@gmail.com>
2024-05-23 20:49:36 +0300

    The first commit. Added .gitignore

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>Main page</h1>')
