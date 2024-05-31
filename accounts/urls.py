310f7d2acff19deb59782f0750d3692938687513
Liza Herasymenko <leonillaeva@gmail.com>
2024-05-31 15:31:09 +0300

    49, add static settings media_url, media_root

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
+    path("create-profile/", views.CreateProfileView.as_view(), name="profile_create"),
     path(
         "activate/<str:username>/<str:token>/",
         views.ActivateAccountView.as_view(),
--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # users
+    'accounts.middleware.ProfileCompletionMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
